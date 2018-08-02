1a51ea57ac76511907cc75f50e0ec22c321aa2c0
Danny Pifer <piferdg@gmail.com>
2018-08-02 10:55:15 -0600

    Adding fully working CRUD code

--- app.js
+++ app.js
@@ -2,29 +2,48 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const morgan = require('morgan')
 const queries = require('./queries')
 const port = parseInt(process.env.PORT || 3000)
 
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(morgan('short'))
 
 app.get('/', (request, response) => {
   queries.getCohorts()
   .then(students => response.json({students}))
 })
 
-app.get('/:id', (request, response) => {
+app.get('/:id', (request, response, next) => {
   queries.getId(request.params.id)
   .then(student => {
     response.json(student)
   })
+  .catch(next)
 })
 
-// app.use((error, request, response, next) => {
-//  console.error('ERROR', error)
-//  response.status(404).send()
-// })
+app.post('/', (request, response, next) => {
+  queries.createStudent(request.body)
+  .then(student => {
+    response.status(201).json({student})
+  })
+  .catch(next)
+})
+
+app.put('/:id', (request, response, next) => {
+  queries.updateStudents(request.params.id, request.body).then(students => {
+      response.json({'students': students[0]});
+  })
+})
+
+app.delete('/:id', (request, response, next) => {
+  queries.deleteStudent(request.params.id)
+  .then(() => response.sendStatus(204))
+})
+
+
 
 app.listen(port, () => {
 console.log(`Listening on Port ${port}`)})
--- queries.js
+++ queries.js
@@ -3,10 +3,27 @@ const database = require('./database-connection')
 module.exports = {
 
   getCohorts() {
-    return database('students').select()
+    return database('students').select().orderBy('id', 'asc')
   },
 
   getId(id) {
     return database('students').select().where('id', id)
+  },
+
+  createStudent(students) {
+    return database('students').insert(students).returning('*')
+    .then(record => record[0])
+
+  },
+
+  updateStudents(id, students) {
+    return database('students').update(students).where('id', id).returning('*')
+    .then(record => record[0])
+    
+  },
+
+  deleteStudent(id) {
+    return database('students').delete().where('id', id)
   }
+
 }
